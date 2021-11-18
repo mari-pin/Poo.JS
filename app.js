a4442e711c6891d7ae3ee64bfb79fa34e18b9953
Maria Ibañez <mirtraffik@gmail.com>
2021-11-18 09:15:38 +0100

    cards dinamicas

--- app.js
+++ app.js
@@ -2,13 +2,43 @@
 //Capturamos todos lo id
 const formulario = document.querySelector(`#formulario`);
 const cardsEstudiantes = document.querySelector(`#cardsEstudiantes`);
-const cardsProfesores = document.querySelector(`#cardsProfesores `);
-const templateEstudiante = document.querySelector(`#templateEstudiante `).content;
+const cardsProfesores = document.querySelector(`#cardsProfesores`);
+const templateEstudiante = document.querySelector(`#templateEstudiante`).content;
 const templateProfesor = document.querySelector(`#templateProfesor`).content;
 
 
 const estudiantes = [];
 const profesores = [];
+
+//detccion del evento click
+document.addEventListener("click", (e)=>{
+    //console.log(e.target.dataset.nombre);
+    if(e.target.dataset.nombre){
+        //console.log(e.target.matches(".btn-success"))
+        if(e.target.matches(".btn-success")){
+            estudiantes.map((item) =>{
+                console.log(e.target.matches(".btn-success"))
+                if(item.nombre === e.target.dataset.nombre){
+                    item.setEstado = true;
+                }
+                console.log(item);
+                return item;
+            });
+        }
+        if(e.target.matches(".btn-danger")){
+            estudiantes.map((item) =>{
+                if(item.nombre === e.target.dataset.nombre){
+                    item.setEstado = false;
+                }
+                console.log(item);
+                return item;
+            });
+        }
+        Persona.pintarPersonaUI(estudiantes, "Estudiante");
+
+    }
+
+})
 
 class Persona {
     constructor(nombre,edad){
@@ -26,7 +56,7 @@ class Persona {
         
         cardsEstudiantes.appendChild(fragment);
         }
-        if (tipo === "Profesro"){
+        if (tipo === "Profesor"){
              cardsProfesores.textContent = "";
              const fragment = document.createDocumentFragment();
 
@@ -51,9 +81,31 @@ class Estudiante extends Persona{
     get getEstudiante (){
          return this.#estudiante;
     }
-    agregaNuevoEstudiante(){
+    //logica de la card estudiantes 
+    agregarNuevoEstudiante(){
         const clone = templateEstudiante.cloneNode(true);
         clone.querySelector("h5 .text-secondary").textContent = this.nombre;
+        clone.querySelector('h6').textContent = this.getEstudiante;
+        clone.querySelector('.lead').textContent = this.edad;
+
+        /* para k cambie el btn  de color al lado del nombre  */
+        if (this.#estado) {
+            clone.querySelector('.badge').className = "badge bg-succes";
+            //activar y desactivar boton aprovado
+            clone.querySelector ('.btn-success').disabled = true;
+            clone.querySelector ('.btn-danger').disabled = false;
+        }else{
+            clone.querySelector('.badge').className = "badge bg-danger"
+            //activar y desactivar boton suspendido
+            clone.querySelector ('.btn-danger').disabled = true;
+            clone.querySelector ('.btn-success').disabled = false;
+        }
+        //btn estado del estudiante
+        clone.querySelector('.badge').textContent = this.#estado ? "Aprobado" : "Suspendido";
+
+        //evento click boton
+        clone.querySelector(".btn-success").dataset.nombre = this.nombre;
+        clone.querySelector(".btn-danger").dataset.nombre = this.nombre;
 
         return clone;
     }
@@ -64,9 +116,10 @@ class Profesor extends Persona {
     #profesor = "Profesor";
     agregarNuevoProfesor(){
         const clone = templateProfesor.cloneNode(true);
-        clone.querySelecto('h5').textContent = this.nombre;
-        clone.querySelecto('h6').textContent = this. #profesor;
+        clone.querySelector('h5').textContent = this.nombre;
+        clone.querySelector('h6').textContent = this. #profesor;
         clone.querySelector('.lead').textContent = this.edad;
+        
     
         return clone;
     }
@@ -76,11 +129,11 @@ class Profesor extends Persona {
 };
 formulario.addEventListener("submit", (e) =>{
     e.preventDefault();
-
+   // console.log('hola');
     const datos = new FormData(formulario);
     const [nombre, edad, opcion] = [...datos.values()];
-   
-    if (opcion === "Estudiantes") {
+    //console.log(opcion);
+    if (opcion === "Estudiante") {
         const estudiante = new Estudiante (nombre, edad);
         estudiantes.push(estudiante);
         Persona.pintarPersonaUI(estudiantes, opcion);
